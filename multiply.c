9a5733c31a9c4c71adf67f8329e43061e5fd4364
Hoyeon Lee <vague0213@gmail.com>
2019-08-14 08:43:06 -0900

    multiply: Add calculations with each digit
    Signed-off-by: Hoyeon Lee <vague0213@gmail.com>

--- multiply.c
+++ multiply.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	int digits[3];
+	int digits[3], calc[3];
 	int operand1, operand2;
 	int idx = 0;
 
@@ -10,9 +10,14 @@ int main() {
 
 	while (operand2 > 0) {
 		digits[idx] = operand2 % 10;
+		calc[idx] = digits[idx] * operand1;
 		operand2 = operand2 / 10;
 		idx = idx + 1;
 	}
+
+	printf("%d\n", calc[0]);
+	printf("%d\n", calc[1]);
+	printf("%d\n", calc[2]);
 
 	return 0;
 }
